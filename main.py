061908b80baea036eccc31006573bfe075975306
Krystian Lalik <klalik27@gmail.com>
2021-06-01 20:39:05 +0200

    Dopracowano sieć neuronową i zakończono pracę nad jej implementacją

--- main.py
+++ main.py
@@ -63,99 +63,77 @@ class IntelligentRadio:
 
 class NeuralNetwork:
     # implementation based on https://www.pyimagesearch.com/2021/05/06/backpropagation-from-scratch-with-python/
-
     DEBUG = True
 
     def __init__(self, layers):
         np.random.seed(1)
-
         self.weights = []
-        self.layers = layers  # List of ints representing network's architecture
-
-
+        self.layers = layers  # List of ints representing network's architecture (number of nodes on every layer)
 
         for i in range(len(layers) - 2):
-            w = np.random.randn(layers[i] + 1, layers[i+1] + 1)  # MxN matrix that connects every node in current layer to every node in next layer
+            w = np.random.randn(layers[i] + 1, layers[i+1] + 1)  # MxN matrix that connects every node in current layer to every node in next layer; +1 are for biases
             self.weights.append(w / np.sqrt(layers[i]))  # normalization of variance of each neuron's output
 
         # the last two layers are a special case where the input
-        # connections need a bias term but the output does not
+        # connections need a bias term but the output (the last layer, of index [-1]) does not
         w = np.random.randn(layers[-2] + 1, layers[-1])
         self.weights.append(w / np.sqrt(layers[-2]))
 
     def train(self, X, y, iterations=100000, alpha=0.2):
         print("Starting NN training")
         X = np.c_[X, np.ones((X.shape[0]))]  # c_ joins slice objects to concatenation along the second axis
+        # here we use it insert column of 1s - which is our bias. This approach will make it trainable parameter
+
         #  we do that concatenation because we want to insert column of 1's as biases
         for iteration in range(iterations):
+            for (X_sample, target) in zip(X, y):  # zip joins X and y into tuples
+                self._trainPartial(X_sample, target, alpha)  # train partial works on every individual sample in X, y. One by one these are passed as (X_sample, target) tuples.
 
-            # Pass training set through network
-            output = self.classify(X, addBias=False)
-
-            for (x, target) in zip(X, y):  # zip joins X and y into tuple
-                self._trainPartial(x, target, alpha)  # train partial works on every individual sample in X, y separately
-
-            # Calculate the error rate
-            error = y - output
             if NeuralNetwork.DEBUG:
                 if iteration % 10000 == 0:
                     print(f"On iteration number {iteration}, loss = {self._calculateLoss(X, y)}")
 
     def _trainPartial(self, x, y, alpha):
-        activations = [np.atleast_2d(x)]
-        # feed forward here
-        for layer in range(len(self.weights)):
-            net = activations[layer].dot(self.weights[layer])
-            activations.append(self._SigmoidActivation(net))
+        x = np.array(x, ndmin=2)  # transform x into matrix
 
-        # back propagate here
-        error = activations[-1] - y  # activations[-1] is the last layer - output
-        deltas = [error * self._SigmoidDerivative(activations[-1])]
-        for layer in range(len(activations) - 2, 0, -1):
-            delta = deltas[-1].dot(self.weights[layer].T)
-            delta = delta * self._SigmoidDerivative(activations[layer])
-            deltas.append(delta)
-        deltas = deltas[::-1]  # reverse this list
+        activations = self._feedForward(x)
+
+        deltas = self._backPropagate(activations, y)
 
         # weights update
         for layer in range(len(self.weights)):
             self.weights[layer] += -alpha * activations[layer].T.dot(deltas[layer])
 
+    def _feedForward(self, input_layer):
+        layer_activations = [input_layer]
+        for layer in range(len(self.weights)):
+            current_layer_output = np.dot(layer_activations[layer], self.weights[layer])
+            layer_activations.append(self._SigmoidActivation(current_layer_output))
+        return layer_activations
+
+    def _backPropagate(self, layers_activations, target_output):
+        error = layers_activations[-1] - target_output  # activations[-1] is the last layer - output
+        deltas = [error * self._SigmoidDerivative(layers_activations[-1])]
+        for layer in range(len(layers_activations) - 2, 0, -1):  # moving back in the loop, hence the name
+            delta = np.dot(deltas[-1], self.weights[layer].T)
+            delta = delta * self._SigmoidDerivative(layers_activations[layer])
+            deltas.append(delta)
+        return deltas[::-1]  # reverse this list before returning
 
     def _calculateLoss(self, X, y):
-        y = np.atleast_2d(y)
+        y = np.array(y, ndmin=2)
         classifications = self.classify(X, addBias=False)
         loss = 0.5 * np.sum((classifications - y) ** 2)
-        
         return loss
 
-    # To niepotrzebne w sumie, chyba że trenowanie będzie trwało 50 godzin to może lepiej wtedy zaimplementować
-    def saveNetworkParameters(self, fileName='nn_parameters'):
-        print(f'Saving neural network parame ers to file {fileName}')
-
-    # To niepotrzebne w sumie, chyba że trenowanie będzie trwało 50 godzin to może lepiej wtedy zaimplementować
-    def loadNetworkParameters(self, fileName='nn_parameters'):
-        print(f'Loading neural network parameters from file {fileName}')
-
     def classify(self, inputData, addBias=True):
-        classification = np.atleast_2d(inputData)
+        classification = np.array(inputData, ndmin=2)
         if addBias:
             classification = np.c_[classification, np.ones((classification.shape[0]))]
 
         for layer in range(len(self.weights)):
             classification = self._SigmoidActivation(np.dot(classification, self.weights[layer]))
         return classification
-
-    def _feedForward(self, inputData):
-        output = self._SigmoidActivation(np.dot(inputData, self.weights))
-        #output = self._ReLuActivation(np.dot(inputData, self.weights)) #  works far worse in 1 layer case, will probably be superior when working with more
-        return output
-
-    def _backPropagate(self):
-        pass
-
-    def _ReLuActivation(self, x):
-        return np.maximum(0, x)
 
     def _SigmoidActivation(self, x):
         return 1 / (1 + np.exp(-x))
@@ -167,6 +145,14 @@ class NeuralNetwork:
         expValues = np.exp(x - np.max(x, axis=1, keepdims=True))
         output = expValues / np.sum(expValues, axis=1, keepdims=True)
         return output
+
+    # To niepotrzebne w sumie, chyba że trenowanie będzie trwało 50 godzin to może lepiej wtedy zaimplementować
+    def saveNetworkParameters(self, fileName='nn_parameters'):
+        print(f'Saving neural network parame ers to file {fileName}')
+
+    # To niepotrzebne w sumie, chyba że trenowanie będzie trwało 50 godzin to może lepiej wtedy zaimplementować
+    def loadNetworkParameters(self, fileName='nn_parameters'):
+        print(f'Loading neural network parameters from file {fileName}')
 
 #
 # class NeuronsLayer:
@@ -206,7 +192,8 @@ if __name__ == '__main__':
         print("Starting KL part test")
 
         def test1_simple_one_layer_nn():
-            print("\nTesting NN with 4 inputs and 1 output. We want NN to output 1 whenever the rightmost of 4 input bits is 1.\n")
+            print(f"\nStarting test1 - {test1_simple_one_layer_nn.__name__}")
+            print("Testing NN with 4 inputs and 1 output. We want NN to output 1 whenever the rightmost of 4 input bits is 1.\n")
             # For some reason there is problem when trying to make NN learn XOR:
             # http://home.agh.edu.pl/~vlsi/AI/xor_t/en/main.html
 
@@ -220,7 +207,7 @@ if __name__ == '__main__':
                                    [1, 1, 1, 0] ])
             trainingy = np.array([[0, 1, 1, 0, 1, 0, 0, 0]]).T
 
-            testNN = NeuralNetwork([4, 1])
+            testNN = NeuralNetwork([len(trainingX[0]), 1])
             print(f"Weights at the beginning: {testNN.weights}")
             output = testNN.classify(trainingX)
             prettyPrintResults(trainingy, output)
@@ -241,7 +228,8 @@ if __name__ == '__main__':
             prettyPrintResults(testy, output)
 
         def test2_parametrized_w_hidden_layer_nn():
-            print("\nTesting neural network number 1 - infamous 3 input XOR which so horribly failed to be trained to 1-layer NN in test 1.\n")
+            print(f"\nStarting test2 - {test2_parametrized_w_hidden_layer_nn.__name__}")
+            print("Testing neural network number 1 - infamous 3 input XOR which so horribly failed to be trained to 1-layer NN in test 1.\n")
             trainingX = np.array([[0, 0, 0],
                                   [0, 0, 1],
                                   [0, 1, 0],
@@ -252,7 +240,7 @@ if __name__ == '__main__':
                                   [1, 1, 1]])
             trainingy = np.array([[0, 1, 1, 0, 1, 0, 0, 1]]).T
 
-            testNN = NeuralNetwork([3, 4, 1])
+            testNN = NeuralNetwork([len(trainingX[0]), 4, 1])
             testNN.train(trainingX, trainingy, iterations=30001)
 
             output = testNN.classify(trainingX)
@@ -269,7 +257,7 @@ if __name__ == '__main__':
                                   [1, 1, 1, 0]])
             trainingy = np.array([[0, 1, 1, 0, 1, 0, 0, 0]]).T
 
-            testNN = NeuralNetwork([4, 3, 4, 1])
+            testNN = NeuralNetwork([len(trainingX[0]), 3, 4, 1])
             testNN.train(trainingX, trainingy, iterations=30001)
 
             testX = np.array([[0, 0, 0, 1],
@@ -282,8 +270,18 @@ if __name__ == '__main__':
             output = testNN.classify(testX)
             prettyPrintResults(testy, output)
 
+        def test3_MNIST_classification_nn():
+            print(f"\nStarting test3 - {test3_MNIST_classification_nn.__name__}")
+            print("\n Testing neural network number 3 - MNIST dataset with much larger NN than all the previous times.\n")
+            from sklearn.model_selection import train_test_split
+            from sklearn import datasets
+
+            digits = datasets.load_digits  # load MNIST dataset
+
+
         test1_simple_one_layer_nn()
-        #test2_parametrized_w_hidden_layer_nn()
+        test2_parametrized_w_hidden_layer_nn()
+        test3_MNIST_classification_nn()
 
 
 
